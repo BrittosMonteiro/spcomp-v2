5613ab8a8d00190fa6427f404205985395a7e9be
brittosmonteiro <brittosmonteiro@gmail.com>
2023-02-15 16:43:43 -0300

    Fixing api route

--- src/services/_config.js
+++ src/services/_config.js
@@ -1,9 +1,9 @@
 let API_URL = "";
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  API_URL = "http://localhost:5050/";
+  API_URL = "http://localhost:5050";
 } else {
-  API_URL = "https://server-to-do-react.onrender.com/";
+  API_URL = "https://spcomp-server.onrender.com";
 }
 
 export default API_URL;
